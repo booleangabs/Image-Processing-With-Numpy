81d8b8b8805b36c8998dcad4d5a665d0378e1332
booleangabs <62074916+booleangabs@users.noreply.github.com>
2021-10-10 16:26:34 -0300

    Histogram equalization

--- histogram.py
+++ histogram.py
@@ -29,7 +29,21 @@ class Histogram:
             for i in range(256):
                 self.hist[i] /= self.pixel_count
             self.is_norm = True
-            
+
+def equalize(image: np.array):
+    hist = Histogram(image).hist
+    result = image.copy()
+    L = 256
+    cumulative_sum = np.zeros((L, ))
+    transform = np.zeros((L, ))
+    cumulative_sum[0] = hist[0]
+    transform[0] = cumulative_sum[0] * (L - 1)
+    image[image == 0] = transform[0] 
+    for i in range(1, L):
+        cumulative_sum[i] = hist[i] + cumulative_sum[i - 1]
+        transform[i] = cumulative_sum[i] * (L - 1)
+        result[result == i] = transform[i]
+    return result.astype('uint8')
         
         
         

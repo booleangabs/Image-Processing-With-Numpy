93820480f2884cfa9816ccb6bc570f99c689ff66
booleangabs <62074916+booleangabs@users.noreply.github.com>
2021-10-27 23:26:46 -0300

    Update intensity_transforms.py

--- intensity_transforms.py
+++ intensity_transforms.py
@@ -1,10 +1,13 @@
 import numpy as np
+from image import mapToRange
+import warnings
+warnings.filterwarnings('ignore')
 
 def negative(image: np.ndarray) -> np.ndarray:
     return 255 - image
 
 def log(image: np.ndarray, c: float=1) -> np.ndarray:
-    return c * np.log(1 + image)
+    return mapToRange(c * np.log(1 + image + 1e15), 0, 255)
 
 def gamma(image: np.ndarray, gamma: float, c: float=1) -> np.ndarray:
-    return c * (image ** gamma)
+    return mapToRange(c * ((image + 1e10) ** gamma), 0, 255)

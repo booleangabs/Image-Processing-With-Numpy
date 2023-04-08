8653c16f8af27ce65f16dc26913afbca856153b6
booleangabs <gabrieltavares1303@gmail.com>
2023-04-08 01:29:40 -0300

    other(examples): Added color conversion example (to be updated)

---
+++ examples/3_color_conversion.py
@@ -0,0 +1,45 @@
+"""
+MIT License
+
+Copyright (c) 2021 booleangabs
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+# local
+from context import image_processing as ipn
+import matplotlib.image as mpimg
+
+
+img = ipn.read_image("inputs/peppers3.tif", ipn.READ_COLOR)
+img_mono = mpimg.imread("inputs/peppers2.tif")[..., 0]
+
+img_bgr = ipn.convert_color(img, ipn.COLOR_RGB2BGR)
+img_recon = ipn.convert_color(img_bgr, ipn.COLOR_BGR2RGB)
+print(f"BGR conversion error (MSE): {((img - img_recon)**2).mean()}")
+ipn.show(img_bgr)
+
+img_gray = ipn.convert_color(img, ipn.COLOR_RGB2GRAY)
+print(f"Grayscale conversion error (MSE): {((img_mono - img_gray)**2).mean()}")
+ipn.show(img_gray)
+
+img_hsv = ipn.convert_color(img, ipn.COLOR_RGB2HSV)
+img_recon = ipn.convert_color(img_hsv, ipn.COLOR_HSV2RGB)
+print(f"HSV conversion error (MSE): {((img - img_recon)**2).mean()}")
+ipn.show(img_hsv)
